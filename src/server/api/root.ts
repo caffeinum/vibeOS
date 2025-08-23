bcb290d3b105ec63b2705905e1b8a86268b97a76
Aleksey Bykhun <theisease@gmail.com>
2025-08-23 01:10:16 -0700

    refactor: remove old click router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,9 @@
 import { router } from './trpc';
-import { clickRouter } from './routers/click';
 import { filesRouter } from './routers/files';
 import { gcpRouter } from './routers/gcp';
 import { terminalRouter } from './routers/terminal';
 
 export const appRouter = router({
-  click: clickRouter,
   files: filesRouter,
   gcp: gcpRouter,
   terminal: terminalRouter,
